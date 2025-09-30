cf09eaadd261855ab2007c6f14197a5c37b08e0d
FantonArthur <e2359875@cmaisonneuve.qc.ca>
2025-09-30 16:46:38 -0400

    Début du site, Sections, style et variables

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Prof Pivot</title>
+</head>
+<body>
+  <h1>Bienvenue sur Prof Pivot</h1>
+  <p>Ceci est la page d'accueil de votre projet web.</p>
+</body>
+</html>
