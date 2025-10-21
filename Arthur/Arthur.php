84551ba6a4cce364947016ea4eb2068aea4759ec
FantonArthur <e2359875@cmaisonneuve.qc.ca>
2025-10-21 16:48:41 -0400

    debut, ajout des grandes lignes

--- Arthur/Arthur.php
+++ Arthur/Arthur.php
@@ -1,3 +1,20 @@
 <section class="Profs">
-  
+  <section class="pivots">
+  <h1>Profs pivots</h1>
+  <div>
+    <div class="prof">
+      <img src="<?php echo get_template_directory_uri(); ?>/Arthur/images/arthur.jpg" alt="Arthur">
+      <h2>David Ross</h2>
+    </div>
+    <div class="prof">
+      <img src="<?php echo get_template_directory_uri(); ?>/Arthur/images/justin.jpg" alt="Justin">
+      <h2>Gregory Bony</h2>
+    </div>
+  </div>
+  </section>
+
+  <section class="aide">
+  <h1>Soutien et encadrement</h1>
+  </section>
+
 </section>
