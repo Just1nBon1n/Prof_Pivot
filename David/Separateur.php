35e0014773537cbd8643440ecc20ce3734cca52b
DavidMarier <144372590+DavidMarier@users.noreply.github.com>
2025-10-23 17:40:21 -0400

    Couleurs dossiers

--- David/Separateur.php
+++ David/Separateur.php
@@ -1,6 +1,12 @@
-<div class="grand-conteneur-separateur">
-    <h2 class="titre-section">Titre</h2>
-</div>
+<?php
+$args = wp_parse_args($args, array(
+    'id' => 'default-separator',
+    'title' => 'Default Title',
+));
+?>
 
+<div class="grand-conteneur-separateur" id="<?php echo esc_attr($args['id']); ?>">
+    <h2 class="titre-section"><?php echo esc_html($args['title']); ?></h2>
+</div>
 
 <script src="<?php echo get_template_directory_uri(); ?>/js/fonctions_separateur.js"></script>
--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 <?php get_template_part('justin/acceuil'); ?>
 
 <!-- <section class="Pixel PixelsB"></section> -->
-<?php get_template_part('David/Separateur'); ?>
+<?php get_template_part('David/Separateur', null, array('id' => 'separateur-1', 'title' => 'Separateur 1'));?>
 <section id="Imp" class="Liens"></section>
 <!-- <section class="Pixel PixelsO"></section> -->
-<?php get_template_part('David/Separateur'); ?>
+<?php get_template_part('David/Separateur', null, array('id' => 'separateur-2', 'title' => 'Separateur 2'));?>
 <section id="Imp" class="Profs"></section>
 <!-- <section class="Pixel Pixelsv"></section> -->
-<?php get_template_part('David/Separateur'); ?>
+<?php get_template_part('David/Separateur', null, array('id' => 'separateur-3', 'title' => 'Separateur 3'));?>
 <section id="Imp" class="Documents">
     <?php get_template_part('David/DocumentsTelechargeables'); ?>
 </section>
