7cc3be449e8bf37bab4fd6fde4ca56425d8082af
BEJohnnyTan <quochungtan@gmail.com>
2025-10-23 17:42:14 -0400

    intégration du test dans le site

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 <?php get_template_part('justin/acceuil'); ?>
 
 <section class="Pixel PixelsB"></section>
-<section id="Imp" class="Liens"></section>
+<section id="Imp" class="Liens">
+<?php get_template_part('Johnny/johnny'); ?>
+
+</section>
 <section class="Pixel PixelsO"></section>
 <section id="Imp" class="Profs"></section>
 <section class="Pixel Pixelsv"></section>
