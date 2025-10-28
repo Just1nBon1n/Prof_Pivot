f7f7bb7654ad70dce6847b1cd4df9d48800f6880
Just1nBon1n <e2263742@cmaisonneuve.qc.ca>
2025-10-28 14:29:22 -0400

    Changement section Johnny

--- index.php
+++ index.php
@@ -4,14 +4,13 @@
 <?php get_template_part('justin/acceuil'); ?>
 
 <section class="Pixel PixelsB"></section>
-<section id="Imp" class="Liens">
-<?php get_template_part('Johnny/johnny'); ?>
-
-</section>
 <section class="Pixel PixelsO"></section>
 <!-- <section class="Pixel PixelsB"></section> -->
 <?php get_template_part('David/Separateur', null, array('id' => 'separateur-1', 'title' => 'LIENS UTILES'));?>
 <section id="Imp" class="Liens"></section>
+<section id="Imp" class="Liens">
+    <?php get_template_part('Johnny/johnny'); ?>
+</section>
 <!-- <section class="Pixel PixelsO"></section> -->
 <?php get_template_part('David/Separateur', null, array('id' => 'separateur-2', 'title' => 'PERSONNES RESSOURCES'));?>
 <section id="Imp" class="Profs"></section>
