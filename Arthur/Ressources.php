b2e4d668b9dd210cf687aadc8ff843e7272aa58a
FantonArthur <e2359875@cmaisonneuve.qc.ca>
2025-11-24 16:21:48 -0500

    carroussel presque fini, manque la rotation et mettre des boutons

--- Arthur/Ressources.php
+++ Arthur/Ressources.php
@@ -14,18 +14,12 @@
   </section>
   <section class="aide">
   <h2 class="res-titre">Soutien et encadrement</h2>
-  <div class="carrousel">
-    <div class="group">
-      <div class="card">API</div>
-      <div class="card">CÉTIM</div>
-      <div class="card">COORDO</div>
-      <div class="card">COORDO2</div>
-    </div>
-    <div aria-hidden class="group">
-      <div class="card">API</div>
-      <div class="card">CÉTIM</div>
-      <div class="card">COORDO</div>
-      <div class="card">COORDO2</div>
+  <div class="banner">
+    <div class="slider" style="--quantity: 4">
+      <div class="item" style="--position: 1"><h2>API</h2></div>
+      <div class="item" style="--position: 2"><h2>Cétim</h2></div>
+      <div class="item" style="--position: 3"><h2>Coodo 1</h2></div>
+      <div class="item" style="--position: 4"><h2>Coordo 2</h2></div>
     </div>
   </div>
   </section>
