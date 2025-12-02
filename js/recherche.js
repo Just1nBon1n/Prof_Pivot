189eb1544e9b315f851167b6b10c074d3d982f2d
Johnny Tan <quochungtan@gmail.com>
2025-12-02 17:09:51 -0500

    Création de la barre de recherche intéractive (il va manquer à lui faire fonctionner)

--- js/recherche.js
+++ js/recherche.js
@@ -1,15 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const searchBar = document.querySelector(".search-bar");
+const toggleSearch = () => {
+  const searchForm = document.querySelector('.search-form');
+  const searchButton = document.querySelector('.search-button');
+  const searchInput = document.querySelector('.search-input');
 
-    if (!searchBar) return;
+  let scrollTimeout;
+  let isClicked = false;
 
-    searchBar.addEventListener("click", (e) => {
-        e.stopPropagation();
-        searchBar.classList.toggle("open");
-    });
+  //Élément du click pour ouvrir et fermer
+  searchButton.addEventListener('click', () => {
+    searchForm.classList.toggle('active-search');
+    isClicked = searchForm.classList.contains('active-search');
+    searchForm.classList.add('visible'); // toujours visible quand clické
+  });
 
-    // Ferme quand on clique ailleurs
-    document.addEventListener("click", () => {
-        searchBar.classList.remove("open");
-    });
-});
+  //Enter 
+  searchInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      searchInput.value = '';
+      searchForm.classList.remove('active-search');
+      isClicked = false;
+    }
+  });
+
+  // Cacher et montrer la barre de recherche 
+  window.addEventListener('scroll', () => {
+    if (!isClicked) {
+      searchForm.classList.add('visible');
+    }
+
+    clearTimeout(scrollTimeout);
+
+    scrollTimeout = setTimeout(() => {
+      if (!isClicked) {
+        searchForm.classList.remove('visible');
+      }
+    }, 1000); //Cacher la barre de recherche après une seconde 
+  });
+};
+
+toggleSearch();
