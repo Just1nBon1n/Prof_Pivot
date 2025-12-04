b9ab251f4c75526a8d2bc4d66e397e41a5f716ae
DavidMarier <144372590+DavidMarier@users.noreply.github.com>
2025-12-04 15:36:17 -0500

    Update optimisation canevas

---
+++ David/SeparateurCanevas.php
@@ -0,0 +1,12 @@
+<canvas></canvas>
+
+<script src="<?php echo get_template_directory_uri(); ?>/js/fonctions_canevas_separateur.js" defer></script>
+
+<style>
+    canvas {
+        display: block;
+        position: relative;
+        width: 100vw;
+        height: 33vh;
+    }
+</style>
