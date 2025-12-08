59d8e773e2e1461413f313f6fc0daa3d81789341
Johnny Tan <quochungtan@gmail.com>
2025-12-07 20:42:30 -0500

    Continuation page 404

--- 404.php
+++ 404.php
@@ -2,7 +2,8 @@
 
 <Section class = "page_404">
     <div class = 404_container></div>
-        <h1>404 - Cette section n'existe pas</h1>
+        <h1>404</h1>
+        <h2>Cette section n'existe pas</h2>
         <a class = "btn_retourner" href="<?php echo home_url(); ?>">
             Retourner à l'acceuil
         </a>
